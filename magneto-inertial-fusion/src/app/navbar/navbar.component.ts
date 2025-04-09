f1143445b6cee53d14d0fc5cab7b07d36246f471
David Overman <portalerd@icloud.com>
2025-04-08 21:14:36 -0700

    Add dropdown for navbar components

--- magneto-inertial-fusion/src/app/navbar/navbar.component.ts
+++ magneto-inertial-fusion/src/app/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [BsDropdownModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  navTo(url: string) {
+    window.location.href = url;
+  }
 }
