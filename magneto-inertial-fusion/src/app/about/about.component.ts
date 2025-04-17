7b91bd797404cfa49763b7fc1b5f3751a38184c7
David Overman <portalerd@icloud.com>
2025-04-16 21:54:49 -0700

    Add about page content

--- magneto-inertial-fusion/src/app/about/about.component.ts
+++ magneto-inertial-fusion/src/app/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from "../page-container/page-container.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [PageContainerComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
