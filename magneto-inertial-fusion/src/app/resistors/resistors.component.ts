11fe0d909701ec78cc90813924e43a6b8d09b89e
David Overman <portalerd@icloud.com>
2025-04-18 13:37:14 -0700

    Add content for resistors page

--- magneto-inertial-fusion/src/app/resistors/resistors.component.ts
+++ magneto-inertial-fusion/src/app/resistors/resistors.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from '../page-container/page-container.component';
+import { ShowcaseComponent } from '../showcase/showcase.component';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-resistors',
-  imports: [],
+  imports: [PageContainerComponent, ShowcaseComponent],
   templateUrl: './resistors.component.html',
   styleUrl: './resistors.component.css'
 })
 export class ResistorsComponent {
-
+  constructor(public navService: NavigationService) {}
 }
