227905a7cdf8b79d445d8c0e7a8826c0706f19db
David Overman <portalerd@icloud.com>
2025-04-18 00:51:08 -0700

    Fill Capacitors + Diodes pages

--- magneto-inertial-fusion/src/app/diodes/diodes.component.ts
+++ magneto-inertial-fusion/src/app/diodes/diodes.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from '../page-container/page-container.component';
+import { ShowcaseComponent } from '../showcase/showcase.component';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-diodes',
-  imports: [],
+  imports: [PageContainerComponent, ShowcaseComponent],
   templateUrl: './diodes.component.html',
   styleUrl: './diodes.component.css'
 })
 export class DiodesComponent {
-
+  constructor (public navService: NavigationService) {}
 }
